253e922ece9130b56f32b802106f9474fab141f2
Hiroyuki Aoki <aokiplayer@icloud.com>
2020-01-28 17:20:43 +0900

    Update to Xcode 11

--- ChildWithoutSegue/ChildWithoutSegue/ViewController.swift
+++ ChildWithoutSegue/ChildWithoutSegue/ViewController.swift
@@ -6,7 +6,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var segment: UISegmentedControl!
 
     @IBAction func showButtonTapped(_ sender: UIButton) {
-        guard let childVC = childViewControllers.first as? ChildViewController else { return }
+        guard let childVC = children.first as? ChildViewController else { return }
 
         childVC.word = segment.titleForSegment(at: segment.selectedSegmentIndex)
     }
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         let sb = UIStoryboard(name: "Main", bundle: nil)
         guard let childVC = sb.instantiateViewController(withIdentifier: "childVC") as? ChildViewController else { return }
 
-        addChildViewController(childVC)
+        addChild(childVC)
         childVC.view.frame = container.bounds
         container.addSubview(childVC.view)
     }
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Sample for passing data to a child view controller. It doesn't use segue.
 
 ## Requirements
-- Xcode 9.x (iOS 11.x, Swift 4.x)
+- Xcode 11.x (iOS 11.x, Swift 5.x)
 
 ## Components in this sample
 ### View
